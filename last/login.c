b3786997909e8d821dc614727ceda9453556c4aa
brcamp13 <brandon.camp.bell@outlook.com>
2019-11-12 17:13:41 -0800

    Update login with comments

--- last/login.c
+++ last/login.c
@@ -10,5 +10,14 @@ main(int argc, char *argv[])
     // settty(argv[1]) set tty name string in proc.tty
     // open /etc/passd file for read
     // while  1
-    //
+    // Printf("login") gets(name);
+    // printf("password: ") gets (password);
+    // for each line in /etc/passwd file do 
+    // tokenize user account line
+    // if user has a valid account
+    // change uid, gid to user's uid, gid; //chuid()
+    // change cwd to user's home DIR // chdir()
+    // close opened /etc/passwd file // close()
+    // exec to program in user account // exec()
+    // else, print that the login failed
 }
