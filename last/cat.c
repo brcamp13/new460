ad95a31a412bddb54844c1e4fe4786a68c532488
brcamp13 <brandon.camp.bell@outlook.com>
2019-11-17 17:49:30 -0800

    Do cat

---
+++ last/cat.c
@@ -0,0 +1,30 @@
+#include "ucode.c"
+
+int main(int argc, char *argv[]) {
+    
+
+    char fileBuf[2];
+    int fd = 0;
+    int n;
+
+    // No file to cat, do stdin
+    if (argc == 1) {
+        while (1) {
+            gets(fileBuf);
+            prints(fileBuf);
+        }
+    }
+    else {
+
+        // Open the file, read and display everything
+        fd = open(argv[1], O_RDONLY);
+        while(n = read(fd, fileBuf, 1)) {
+            fileBuf[n] = 0;
+            printf("%s", fileBuf);
+        }
+    }
+
+    close(fd);
+
+    return 0;
+}
