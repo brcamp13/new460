de83f452d2f5952c78fa554dd96ba4c2b61bf128
brcamp13 <brandon.camp.bell@outlook.com>
2019-11-17 18:36:27 -0800

    Add test file

---
+++ last/grep.c
@@ -0,0 +1,10 @@
+#include "ucode.c"
+
+
+int main(int argc, char *argv[]) {
+
+    // So the general idea is to just read input from the file, line-by-line
+    // When you encounter a line with the pattern that you are looking for, then you print that line
+    // Choose 
+    
+}
