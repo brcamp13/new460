df47cee4cdbc8fa58fc15bc7c31d174eb2650e89
brcamp13 <brandon.camp.bell@outlook.com>
2019-11-18 16:04:40 -0800

    Remove bad ls and add new ls main

--- last/ls.c
+++ last/ls.c
@@ -1,93 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <sys/time.h>
-#include <dirent.h>
-#include <errno.h>
 #include "ucode.c"
 
-char *t1 = "xwrxwrxwr-------";
-char *t2 = "----------------";
-struct stat mystat, *sp;
+STAT utat, *sp;
+int fd, n;
+DIR *dp;
+char f[32], cwdname[64], file[64], buf[1024];
+char *cp;
 
-int ls_file(char *fname) {
-    struct stat fstat, *sp, &fstat;
-    int r, i;
-    char sbuf[4096];
 
-    r = lstat(fname, sp);
+int main(int argc, char *argv) {
 
-    if (S_ISDIR(sp->st_mode)) {
-        printf("%c", 'd');
+    sp = &utat;
+    int fileStat;
+
+    printf("==========Brandon LS ==========");
+
+    // Ls the current directory as nothing specified
+    if (argc == 1) {
+        strcpy(file, "./");
     }
-    if (S_ISREG(sp->st_mode)) {
-        printf("%c", '-');
-    }
-    if (S_ISLINK(sp->st_mode)) {
-        printf("%c", 'l');
-    }
-
-    for (i = 8; i >= 0; i--) {
-        if (sp->st_mode & (1<<i)) {
-            printf("%c", t1[i]);
-        }
-        else {
-            printf("%c", t2[i]);
-        }
+    else { // Ls the filename/dirname that was given by user
+        strcpy(file, argv[1]);
     }
 
-    printf("%4d ", sp->st_nlink);
-    printf("%4d ", sp->st_uid);
-    printf("%8d ", sp->st_size);
-    printf("%s", fname);
-    
-}
+    fileStat = stat(file, sp);
 
-int ls_dir(char *dname) {
-    char name[256];
-    DIR *dp;
-    struct dirent *ep;
-
-    dp = opendir(dname);
-    while (ep = readdir(dp)) {
-        strcpy(name, ep->d_name);
-        if (!strcmp(name, ".")) {
-            continue;
-        }
-        strcpy(name, dname);
-        strcat(name, "/");
-        strcat(name, ep->d_name);
-        ls_file(name);
-    }
-}
-
-int main(int argc, char *argv[]) {
-    struct stat mystat, *sp;
-    int r;
-    char *s;
-    char filename[1024], cwd[1024];
-
-    s = argv[1];
-    if ((r = stat(s, sp)) < 0) {
-        perror("ls");
-        exit(1);
-    }
-
-    strcpy(filename, s);
-    if (s[0] != '/') {
-        getcwd(cwd);
-        strcpy(filename, cwd);
-        strcat(filename, "/");
-        strcat(filename, s);
-    }
-
-    if (S_ISDIR(sp->st_mode)) {
-        ls_dir(filename);
+    // If the mode is a file, then ls file
+    if ((sp->st_mode & 0100000) == 0100000) {
+        ls_file(sp, file, file);
     }
     else {
-        ls_file(filename);
+        // Otherwise, make sure that it's a dir and ls the dir
+        if ((sp->st_mode & 0040000) == 0040000) {
+            ls_dir(sp, file);
+        }
     }
-
 }
